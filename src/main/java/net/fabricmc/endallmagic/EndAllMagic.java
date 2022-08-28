366ab88ddf2049fd411bed0aed5497656b1a9d57
matteobir12 <bir.matteo@gmail.com>
2022-08-28 16:16:15 -0500

    Update EndAllMagic.java

--- src/main/java/net/fabricmc/endallmagic/EndAllMagic.java
+++ src/main/java/net/fabricmc/endallmagic/EndAllMagic.java
@@ -1,10 +1,13 @@
 package net.fabricmc.endallmagic;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.endallmagic.common.Networking;
 import net.fabricmc.endallmagic.common.spells.Spell;
 import net.fabricmc.endallmagic.items.Staff;
 import net.fabricmc.fabric.api.event.registry.FabricRegistryBuilder;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.fabricmc.integration.EndAllMagicConfig;
 import net.minecraft.entity.attribute.ClampedEntityAttribute;
 import net.minecraft.entity.attribute.EntityAttribute;
 import net.minecraft.entity.data.DataTracker;
@@ -18,6 +21,9 @@ import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.JanksonConfigSerializer;
+
 public class EndAllMagic implements ModInitializer {
 	// custom reg public static final Registry<Spell> SPELL = createRegistry("spell", Spell.class);
 
@@ -29,7 +35,13 @@ public class EndAllMagic implements ModInitializer {
 	public static final Staff STAFF = new Staff(new FabricItemSettings().group(ItemGroup.MISC));
 	@Override
 	public void onInitialize() {
+		AutoConfig.register(EndAllMagicConfig.class, JanksonConfigSerializer::new);
 		Registry.register(Registry.ITEM, new Identifier(MOD_ID, "staff"), STAFF);
+
+		ServerPlayNetworking.registerGlobalReceiver(Networking.ID, Networking::handle);
+
+		Registry.register(Registry.ATTRIBUTE, new Identifier(MOD_ID, "mana_regen"), EntityAttributes.MANA_REGEN);
+
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
